4ca8c198ffce4d6a266ff1a966202d47c7a77f4e
Carl Edquist <edquist@cs.wisc.edu>
2017-04-27 09:55:43 -0500

    options

--- shazer.py
+++ shazer.py
@@ -1,10 +1,31 @@
 #!/usr/bin/python
 
+import getopt
 import sys
 import os
 
-dryrun = True
+dryrun = False
 minsize = 1024
+
+def usage():
+    print "usage: %s [-n] [-s minsize] shasfile" % os.path.basename(__file__)
+    sys.exit(0)
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], 'ns:')
+except getopt.GetoptError:
+    usage()
+
+for k,v in opts:
+    if   k == '-n':
+        dryrun = True
+    elif k == '-s':
+        minsize = int(v)
+
+if len(args) != 1:
+    usage()
+
+infile = args[0]
 
 def hashline(line):
     line = line.rstrip('\n')
@@ -20,7 +41,7 @@ firstmap = {}
 firstino = {}
 shrinkage = 0
 
-for mtime,fn,sha,size,ino in sorted(map(hashline, open(sys.argv[1]))):
+for mtime,fn,sha,size,ino in sorted(map(hashline, open(infile))):
     if sha in firstmap:
         if firstino[sha] != ino and size > minsize:
             first = firstmap[sha]
