006ba866f72668c5c8c05f1f16c2706062d8b95f
Carl Edquist <edquist@cs.wisc.edu>
2018-02-08 10:35:19 -0600

    allow for various checksum lengths

--- shazer.py
+++ shazer.py
@@ -108,17 +108,13 @@ def unescape(fn):
     return re.sub(r'\\([\\n])', helper, fn)
 
 def hashline(line):
-    line = line.rstrip('\n')
-    m = line.startswith('\\') and re.match(r'\\([0-9a-f]{40})  (.+)', line)
-    if m:
-        sha, efn = m.groups()
-        fn = unescape(efn)
-    elif re.match(r'[0-9a-f]{40}  .', line):
-        sha = line[:40]
-        fn = line[42:]
-    else:
+    m = re.match(r'(\\)?([0-9a-f]{32,}) [* ](.+)', line)
+    if not m:
         print >>sys.stderr, "skipping malformed line: '%s'" % line
         return
+    esc, sha, fn = m.groups()
+    if esc:
+        fn = unescape(fn)
     if globpat is not None and not globmatch(fn):
         return
     return FInfo(sha, fn)
